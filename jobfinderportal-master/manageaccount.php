77bb9effdd6731cd7c39eca8cd6d1432e1a6a40f
OAB710 <oab.phd@gmail.com>
2024-04-01 15:37:14 +0700

    3:37

--- jobfinderportal-master/manageaccount.php
+++ jobfinderportal-master/manageaccount.php
@@ -69,7 +69,7 @@
                                             </li>
                                             <li><a href="contact.php">Contact</a></li>
                                             <?php
-                                            include ("checksession.php");
+                                            include ("connection/checksession.php");
                                             ?>
                                         </ul>
                                     </nav>
@@ -137,26 +137,6 @@
                 </div>
             </div>
         </div>
-        <div class="pagination-area pb-115 text-center">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-12">
-                        <div class="single-wrap d-flex justify-content-center">
-                            <nav aria-label="Page navigation example">
-                                <ul class="pagination justify-content-start">
-                                    <li class="page-item active"><a class="page-link" href="#">01</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">02</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">03</a></li>
-                                    <li class="page-item"><a class="page-link" href="#"><span
-                                                class="ti-angle-right"></span></a></li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--Pagination End  -->
 
     </main>
     <footer>
