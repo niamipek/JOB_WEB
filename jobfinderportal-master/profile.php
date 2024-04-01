bc7a21e259ad06c2900cb290fd41b138dda861a2
niamipek <161831080+niamipek@users.noreply.github.com>
2024-04-02 00:24:21 +0700

    update

--- jobfinderportal-master/profile.php
+++ jobfinderportal-master/profile.php
@@ -35,6 +35,10 @@
             background-color: #28395a;
             /* Đặt màu nền cho đường viền */
         }
+        a:hover
+        {   color: black;
+            font-weight: bold !important;
+        }
     </style>
 </head>
 
@@ -121,7 +125,7 @@
                                     <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-lock2" viewBox="0 0 16 16">
                                             <path d="M8 5a1 1 0 0 1 1 1v1H7V6a1 1 0 0 1 1-1m2 2.076V6a2 2 0 1 0-4 0v1.076c-.54.166-1 .597-1 1.224v2.4c0 .816.781 1.3 1.5 1.3h3c.719 0 1.5-.484 1.5-1.3V8.3c0-.627-.46-1.058-1-1.224" />
                                             <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1" />
-                                        </svg><a class="changePass" href="index.php">Change password</a></h6>
+                                        </svg><a id="changepassword" href="index.php">Change password</a></h6>
 
                                 </li>
                                 <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
